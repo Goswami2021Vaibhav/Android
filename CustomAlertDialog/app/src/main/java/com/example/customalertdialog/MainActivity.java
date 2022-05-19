b7f9f64a800ff56d37f1dad40d7665b607611fc8
vaibhav goswami <goswamivaibhav72@gmail.com>
2022-05-19 21:54:36 +0530

    Custom Dialog Completed

--- CustomAlertDialog/app/src/main/java/com/example/customalertdialog/MainActivity.java
+++ CustomAlertDialog/app/src/main/java/com/example/customalertdialog/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.customalertdialog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.custom_layout);
+        dialog.setCancelable(false);
+        Button btnok = dialog.findViewById(R.id.btnok);
+        btnok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Closed", Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
     }
 }
