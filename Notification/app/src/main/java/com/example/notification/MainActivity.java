0cec485c0afa3380e373829f1ac5018ed47cd229
vaibhav goswami <goswamivaibhav72@gmail.com>
2022-05-20 20:32:34 +0530

    Updating: Notification

--- Notification/app/src/main/java/com/example/notification/MainActivity.java
+++ Notification/app/src/main/java/com/example/notification/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.notification;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String CHANNEL_ID = "My Channel";
+    private static final int NOTIFICATION_ID = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Drawable drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.pngicon, null);
+        BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+        Bitmap largeIcon = bitmapDrawable.getBitmap();
+
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification notification;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            notification = new Notification.Builder(this)
+                    .setLargeIcon(largeIcon)
+                    .setSmallIcon(R.drawable.pngicon)
+                    .setContentText("New Message")
+                    .setSubText("New Message From Vaibhav")
+                    .setChannelId(CHANNEL_ID)
+                    .build();
+            nm.createNotificationChannel(new NotificationChannel(CHANNEL_ID, "New Channel", NotificationManager.IMPORTANCE_HIGH));
+        } else {
+            notification = new Notification.Builder(this)
+                    .setLargeIcon(largeIcon)
+                    .setSmallIcon(R.drawable.pngicon)
+                    .setContentText("New Message")
+                    .setSubText("New Message From Vaibhav")
+                    .build();
+        }
+        nm.notify(NOTIFICATION_ID, notification);
     }
 }
